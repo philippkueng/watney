029d499b7d08a1714a122730311e4ff26888f00e
Philipp Küng <git@philippkueng.ch>
2015-08-23 18:32:04 +0200

    require enlive and adding a private parse function that returns an enlive tree that is used as a basis

--- src/watney/core.clj
+++ src/watney/core.clj
@@ -1,4 +1,12 @@
-(ns watney.core)
+(ns watney.core
+  (:require [net.cgrand.enlive-html :as html]))
+
+(defn ^:private parse
+  "Parse the given HTML using Enlive and return the tree"
+  [html-string]
+  (-> html-string
+      java.io.StringReader.
+      html/html-resource))
 
 (defn convert
   "Convert the HTML string given into Markdown"
