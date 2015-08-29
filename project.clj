0057cc5ef1a4cc2d31f834070000e34c12f4b025
Philipp Küng <git@philippkueng.ch>
2015-08-29 12:29:20 +0200

    bumped the clojure version used from 1.6.0 to 1.7.0 since otherwise markdown-clj throws namespace errors
    `Caused by: java.io.FileNotFoundException: Could not locate markdown/transformers__init.class or markdown/transformers.clj on classpath:`

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/philippkueng/watney"
   :license {:name "MIT"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [enlive "1.1.6"]]
   :profiles {:dev {:dependencies [[midje "1.7.0" :exclusions [org.clojure/tools.namespace org.clojure/clojure]]
                                   [markdown-clj "0.9.69" :exclusions [org.clojure/clojure]]]
