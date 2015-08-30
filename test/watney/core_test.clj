1911c025f6d0e14356b6d8f23715c1253d1b1865
Philipp Küng <git@philippkueng.ch>
2015-08-30 22:01:31 +0200

    requiring markdown.core for the tests

--- test/watney/core_test.clj
+++ test/watney/core_test.clj
@@ -1,6 +1,7 @@
 (ns watney.core-test
-  (:require [watney.core :as w])
-  (:use midje.sweet))
+  (:require [watney.core :as w]
+            [markdown.core :as md])
+  (:use [midje.sweet]))
 
 (fact "parsing the simple.html file should return a correct simple.md"
   (w/convert (slurp "fixtures/simple.html")) => (slurp "fixtures/simple.md"))
