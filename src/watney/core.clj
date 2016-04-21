1286798c2ad2549d454275b59e4fe3eda8d06edc
Philipp Küng <git@philippkueng.ch>
2016-04-21 07:17:42 +0200

    correctly convert h1-h5 elements containing other things than just text

--- src/watney/core.clj
+++ src/watney/core.clj
@@ -21,42 +21,48 @@
        (map (fn [i] " "))
        (apply str)))
 
+(defn ^:private convert-content
+  "Return the converted contents of an element."
+  [contents prefix-spaces]
+  )
+
 (defn ^:private convert-entity
   "Returns Markdown when given an Enlive data tree."
   [html-tree prefix-spaces]
-  (str (whitespace-str prefix-spaces)
-       (->> html-tree
-            (map (fn [node]
-                   (case (:tag node)
-                     :h1 (str "# " (first (:content node)))
-                     :h2 (str "## " (first (:content node)))
-                     :h3 (str "### " (first (:content node)))
-                     :h4 (str "#### " (first (:content node)))
-                     :p  (str "\n" (->> (:content node)
-                                        (map (fn [sub-node]
-                                               (if (= (type sub-node) java.lang.String)
-                                                 sub-node
-                                                 (->> (convert-entity (list sub-node) prefix-spaces)))))
-                                        (clojure.string/join "")))
-                     :a  (str "[" (first (:content node)) "](" (:href (:attrs node)) ")")
-                     :img (str "![" (:alt (:attrs node)) "](" (:src (:attrs node)) ")")
+  (let [convert-content #(->> (:content %)
+                              (map (fn [sub-node]
+                                     (if (= (type sub-node) java.lang.String)
+                                       sub-node
+                                       (->> (convert-entity (list sub-node) prefix-spaces)))))
+                              (clojure.string/join ""))]
+    (str (whitespace-str prefix-spaces)
+         (->> html-tree
+              (map (fn [node]
+                     (case (:tag node)
+                       :h1 (str "# " (convert-content node))
+                       :h2 (str "## " (convert-content node))
+                       :h3 (str "### " (convert-content node))
+                       :h4 (str "#### " (convert-content node))
+                       :p  (str "\n" (convert-content node))
+                       :a  (str "[" (first (:content node)) "](" (:href (:attrs node)) ")")
+                       :img (str "![" (:alt (:attrs node)) "](" (:src (:attrs node)) ")")
 
-                     ;; if any of the children of this :li is a :ul, then return 2 spaces here
-                     :li (if (contains-node? (:content node) :ul)
-                           (str (if (= (type (first (:content node))) java.lang.String)
-                                  (str "*  " (first (:content node)) "\n")
-                                  nil)
-                                (->> (:content node)
-                                     (filter #(= (:tag %) :ul))
-                                     first
-                                     :content
-                                     (map (fn [entity]
-                                            (convert-entity (list entity) (+ prefix-spaces 2))))
-                                     (str/join "\n")))
-                           (str "*  " (first (:content node))))
-                     (convert-entity (:content node) 0))))
-            (remove empty?)
-            (str/join "\n"))))
+                       ;; if any of the children of this :li is a :ul, then return 2 spaces here
+                       :li (if (contains-node? (:content node) :ul)
+                             (str (if (= (type (first (:content node))) java.lang.String)
+                                    (str "*  " (first (:content node)) "\n")
+                                    nil)
+                                  (->> (:content node)
+                                       (filter #(= (:tag %) :ul))
+                                       first
+                                       :content
+                                       (map (fn [entity]
+                                              (convert-entity (list entity) (+ prefix-spaces 2))))
+                                       (str/join "\n")))
+                             (str "*  " (first (:content node))))
+                       (convert-entity (:content node) 0))))
+              (remove empty?)
+              (str/join "\n")))))
 
 (defn convert
   "Convert the HTML string given into Markdown"
